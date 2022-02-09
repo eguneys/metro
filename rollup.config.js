461995eabc596e0802b373eb3a211823d3916ccc
Emre Guneyler <eguneys@gmail.com>
2022-02-09 04:39:48 +0000

    production

--- rollup.config.js
+++ rollup.config.js
@@ -7,25 +7,38 @@ const image = require('@rollup/plugin-image')
 const serve = require('rollup-plugin-serve')
 const livereload = require('rollup-plugin-livereload')
 
-export default {
-  input: 'src/main.ts',
-  output: [
-    { format: 'iife', name: 'Space', dir: 'dist', sourcemap: true }
-  ],
-  watch: {
-    clearScreen: false
-  },
-  plugins: [
-    nodeResolve(),
-    typescript(),
-    htmlTemplate({
-      template: 'src/index.html',
-      target: 'index.html'
-    }),
-    image(),
-    serve({ contentBase: 'dist', port: 3000 }),
-    livereload({ watch: 'dist', port: 8080 })
-  ]
+const { terser } = require('rollup-plugin-terser')
+
+export default args => {
+  let prod = args['config-prod']
+  return {
+    input: 'src/main.ts',
+    output: {
+      format: 'iife', 
+      name: 'Space',
+      dir: 'dist',
+      ...(prod ? {
+        entryFileNames: '[name].min.js',
+        plugins: [terser()]
+      } : {
+        sourcemap: true
+      })
+    },
+    watch: {
+      clearScreen: false
+    },
+    plugins: [
+      nodeResolve(),
+      typescript(),
+      htmlTemplate({
+        template: 'src/index.html',
+        target: 'index.html'
+      }),
+      image(),
+      ...(prod ? [] : [
+        serve({ contentBase: 'dist', port: 3000 }),
+        livereload({ watch: 'dist', port: 8080 })
+      ])
+    ]
+  }
 }
-
-
